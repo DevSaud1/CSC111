375f099b70c27cb70d2e4d8db053a008648a7cd5
DevSaud1 <saud.aldajani8@gmail.com>
2024-11-28 14:57:56 +0300

    Add files via upload

---
+++ lab01/src/Variables.java
@@ -0,0 +1,15 @@
+public class Variables {
+	
+	public static void main(String[] args) {
+
+		int x, y;
+		double z;
+		
+		x=10; y=20; z=10.15;
+		
+		System.out.println("x * y = "+ (x*y));
+		System.out.println("x * z = "+ (x*z));
+		
+	}
+
+}
